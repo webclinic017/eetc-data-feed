60b0d56a7122773b954681a27d4ce4c0955e37e1
delicmakaveli <delic665@gmail.com>
2018-12-24 23:10:11 +0100

    #4 - Implement multi-threading

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,56 @@
+use std::thread;
+
 extern crate zmq;
 
+// Configuration
+static BFX_DATA_FEED_SUB_ENDPOINT: &str = "tcp://localhost:5555";
+static BFX_DATA_FEED_PUSH_PULL_ENDPOINT: &str = "ipc://bitfinex_data_feed";
+static EETC_DATA_FEED_PUB_ENDPOINT: &str = "tcp://*:4444";
+
 fn main() {
-    // TODO refactor this program to be multithreaded for each microservice it's subbed to
     // TODO implement logging
     println!("Started EETC Data Feed.");
 
+    // Create ZeroMQ Context which will be shared between threads
     let zmq_context:zmq::Context = zmq::Context::new();
 
-    let zmq_sub_socket = zmq_context.socket(zmq::SUB).unwrap();
-    zmq_sub_socket
-        .connect("tcp://localhost:5555")
-        .expect("Could not connect to PUB.");
-    zmq_sub_socket
-        .set_subscribe(b"")
-        .expect("Could not subscribe to topic.");
-
+    // Create ZeroMQ Sockets that we'll use in the main thread
     let zmq_pub_socket = zmq_context.socket(zmq::PUB).unwrap();
+    let zmq_pull_socket = zmq_context.socket(zmq::PULL).unwrap();
+
+    // Bind Sockets to endpoints
+    zmq_pull_socket
+        .bind(BFX_DATA_FEED_PUSH_PULL_ENDPOINT)
+        .expect("Could not bind PULL socket.");
     zmq_pub_socket
-        .bind("tcp://*:4444")
+        .bind(EETC_DATA_FEED_PUB_ENDPOINT)
         .expect("Could not bind PUB socket.");
 
-    zmq::proxy(&zmq_sub_socket, &zmq_pub_socket).expect("ZMQ Proxy Error.");
+    // Spawn Thread for Bitfinex Data Feed
+    thread::spawn(move || {
+        println!("Started Bitfinex Data Feed PUB-SUB Thread.");
+
+        // Create ZeroMQ Sockets that we'll use in this thread & use zmq_context from main thread
+        let zmq_sub_socket = zmq_context.socket(zmq::SUB).unwrap();
+        let zmq_push_socket = zmq_context.socket(zmq::PUSH).unwrap();
+
+        // Connect Sockets to endpoints
+        zmq_push_socket
+            .connect(BFX_DATA_FEED_PUSH_PULL_ENDPOINT)
+            .expect("Could not connect to PULL");
+        zmq_sub_socket
+            .connect(BFX_DATA_FEED_SUB_ENDPOINT)
+            .expect("Could not connect to PUB.");
+
+        // Set subscription topic for SUB Socket
+        zmq_sub_socket
+            .set_subscribe(b"") //  "" is the default value to subscribe to all topics
+            .expect("Could not subscribe to topic.");
+
+        // Forward data from SUB Socket to PUSH Socket, which sends it to the PULL Socket in the main thread
+        zmq::proxy(&zmq_sub_socket, &zmq_push_socket).expect("ZMQ Proxy Error.");
+    });
+
+    // Forward data from PULL Socket to the PUB Socket, to which the clients are subscribed to
+    zmq::proxy(&zmq_pull_socket, &zmq_pub_socket).expect("ZMQ Proxy Error.");
 }
