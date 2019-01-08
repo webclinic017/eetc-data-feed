21167d46a95a99a2101ce7d29ce042bd10511cea
delicmakaveli <delic665@gmail.com>
2019-01-08 22:34:55 +0100

    #7 - Move Thread routines into separate functions

--- src/main.rs
+++ src/main.rs
@@ -26,73 +26,87 @@ fn main() {
         .bind(EETC_DATA_FEED_PUB_ENDPOINT)
         .expect("Could not bind PUB socket.");
 
-    // Spawn Thread for PUB-SUB
+    // Spawn Thread for Bitfinex Data Feed PUB-SUB
     thread::spawn(move || {
         println!("Started Bitfinex Data Feed PUB-SUB Thread.");
-
-        let zmq_sub_socket = zmq_context.socket(zmq::SUB).unwrap();
-        let zmq_push_socket = zmq_context.socket(zmq::PUSH).unwrap();
-
-        zmq_push_socket
-            .connect(EETC_DATA_FEED_PUSH_PULL_ENDPOINT)
-            .expect("Could not connect to PULL");
-        zmq_sub_socket
-            .connect(BFX_DATA_FEED_SUB_ENDPOINT)
-            .expect("Could not connect to PUB.");
-
-        zmq_sub_socket
-            .set_subscribe(b"") //  "" is the default value to subscribe to all topics
-            .expect("Could not subscribe to topic.");
-
-        zmq::proxy(&zmq_sub_socket, &zmq_push_socket).expect("ZMQ Proxy Error.");
+        bitfinex_data_feed_pub_sub_routine(&zmq_context);
     });
 
     // Spawn Thread for REQ-REP for Bitfinex Hist Data Microservice
     thread::spawn(|| {
-        println!("Started Bitfinex Hist Data Microservice Thread.");
-
-        let zmq_context:zmq::Context = zmq::Context::new();
-
-        let zmq_router_socket = zmq_context.socket(zmq::ROUTER).unwrap();
-        let zmq_dealer_socket = zmq_context.socket(zmq::DEALER).unwrap();
-
-        zmq_router_socket
-            .bind(EETC_DATA_FEED_REQ_REP_ENDPOINT_BFX)
-            .expect("Could not bind ROUTER socket.");
-        zmq_dealer_socket
-            .bind(EETC_DATA_FEED_DEALER_ENDPOINT)
-            .expect("Could not bind DEALER socket.");
-
-        // Spawn Worker Threads
-        // TODO refaactor to spawn a worker thread for each request that comes in
-        for i in 1..=BFX_HIST_DATA_MICROSERVICE_THREADS {
-            thread::spawn(|| {
-                let zmq_context:zmq::Context = zmq::Context::new();
-
-                let zmq_rep_socket = zmq_context.socket(zmq::REP).unwrap();
-                let zmq_req_socket = zmq_context.socket(zmq::REQ).unwrap();
-
-                zmq_req_socket
-                    .connect(BFX_DATA_FEED_REQ_REP_ENDPOINT)
-                    .expect("Could not connect to REP");
-                zmq_rep_socket
-                    .connect(EETC_DATA_FEED_DEALER_ENDPOINT)
-                    .expect("Could not connect to DEALER");
-
-                loop {
-                    let request = zmq_rep_socket.recv_bytes(0).unwrap();
-                    zmq_req_socket.send(request, 0).expect("ZMQ Error.");
-
-                    let response = zmq_req_socket.recv_multipart(0).unwrap();
-                    zmq_rep_socket.send_multipart(response, 0).expect("ZMQ Error.");
-                }
-            });
-
-            println!("Started Bitfinex Hist Data Microservice Worker Thread {}.", i);
-        }
-
-        zmq::proxy(&zmq_router_socket, &zmq_dealer_socket).expect("ZMQ Proxy Error");
+        println!("Started Bitfinex Data Feed REQ-REP Thread.");
+        bitfinex_data_feed_req_rep_routine();
     });
 
+    // TODO Spawn Thread for InteractiveBrokers Data Feed PUB-SUB
+
+    // TODO Spawn Thread for InteractiveBrokers Data Feed REQ-REP
+
     zmq::proxy(&zmq_pull_socket, &zmq_pub_socket).expect("ZMQ Proxy Error.");
+}
+
+fn bitfinex_data_feed_pub_sub_routine(zmq_context:&zmq::Context) {
+    let zmq_sub_socket = zmq_context.socket(zmq::SUB).unwrap();
+    let zmq_push_socket = zmq_context.socket(zmq::PUSH).unwrap();
+
+    zmq_push_socket
+        .connect(EETC_DATA_FEED_PUSH_PULL_ENDPOINT)
+        .expect("Could not connect to PULL");
+    zmq_sub_socket
+        .connect(BFX_DATA_FEED_SUB_ENDPOINT)
+        .expect("Could not connect to PUB.");
+
+    zmq_sub_socket
+        .set_subscribe(b"") //  "" is the default value to subscribe to all topics
+        .expect("Could not subscribe to topic.");
+
+    zmq::proxy(&zmq_sub_socket, &zmq_push_socket).expect("ZMQ Proxy Error.");
+}
+
+fn bitfinex_data_feed_req_rep_routine() {
+    let zmq_context:zmq::Context = zmq::Context::new();
+
+    let zmq_router_socket = zmq_context.socket(zmq::ROUTER).unwrap();
+    let zmq_dealer_socket = zmq_context.socket(zmq::DEALER).unwrap();
+
+    zmq_router_socket
+        .bind(EETC_DATA_FEED_REQ_REP_ENDPOINT_BFX)
+        .expect("Could not bind ROUTER socket.");
+    zmq_dealer_socket
+        .bind(EETC_DATA_FEED_DEALER_ENDPOINT)
+        .expect("Could not bind DEALER socket.");
+
+    // Spawn Worker Threads
+    // TODO refaactor to spawn a worker thread for each request that comes in
+    for i in 1..=BFX_HIST_DATA_MICROSERVICE_THREADS {
+        thread::spawn(|| {
+            let zmq_context:zmq::Context = zmq::Context::new();
+
+            let zmq_rep_socket = zmq_context.socket(zmq::REP).unwrap();
+            let zmq_req_socket = zmq_context.socket(zmq::REQ).unwrap();
+
+            zmq_req_socket
+                .connect(BFX_DATA_FEED_REQ_REP_ENDPOINT)
+                .expect("Could not connect to REP");
+            zmq_rep_socket
+                .connect(EETC_DATA_FEED_DEALER_ENDPOINT)
+                .expect("Could not connect to DEALER");
+
+            forward_request(&zmq_rep_socket, &zmq_req_socket);
+        });
+
+        println!("Started Bitfinex Hist Data Microservice Worker Thread {}.", i);
+    }
+
+    zmq::proxy(&zmq_router_socket, &zmq_dealer_socket).expect("ZMQ Proxy Error");
+}
+
+fn forward_request(zmq_rep_socket: &zmq::Socket, zmq_req_socket: &zmq::Socket) {
+    loop {
+        let request = zmq_rep_socket.recv_bytes(0).unwrap();
+        zmq_req_socket.send(request, 0).expect("ZMQ Error.");
+
+        let response = zmq_req_socket.recv_multipart(0).unwrap();
+        zmq_rep_socket.send_multipart(response, 0).expect("ZMQ Error.");
+    }
 }
